513f867ee0790753bde8be9c8eddc8982a2e1e0a
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2016-08-12 15:07:01 -0400

    Destructure array when passing args

--- lib/rubo_claus.rb
+++ lib/rubo_claus.rb
@@ -12,7 +12,7 @@ module RuboClaus
 
     def clauses(*klauses)
       define_method(@function_name) do |*runtime_args|
-        this_function = klauses.find { |pattern| match(pattern, runtime_args) }
+        this_function = klauses.find { |pattern| match?(pattern, runtime_args) }
 
         return this_function.function.call(*runtime_args) if this_function.is_a?(Clause)
         return klauses[-1].proc.call if klauses[-1].is_a?(CatchAll)
@@ -33,17 +33,17 @@ module RuboClaus
     klass.extend ClassMethods
   end
 
-  private def single_match(lhs, rhs)
+  private def single_match?(lhs, rhs)
     return true if lhs == :any
     return true if lhs == rhs
     return true if lhs == rhs.class
     false
   end
 
-  private def match(lhs, rhs)
+  private def match?(lhs, rhs)
     return true if lhs.is_a?(RuboClaus::CatchAll)
     return false if lhs.args.length != rhs.length
-    lhs.args.zip(rhs) { |array| return false unless single_match(array[0], array[1]) }
+    lhs.args.zip(rhs) { |array| return false unless single_match?(*array) }
     true
   end
 end
